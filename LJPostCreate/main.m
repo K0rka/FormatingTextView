b553c84b467cf3861d0dcb5b30fd5d3bc528e2dd
Korovkina Ekaterina <e.korovkina@ekorovkina.local>
2015-05-08 09:17:30 +0300

    Initial Commit

---
+++ LJPostCreate/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LJPostCreate
+//
+//  Created by Korovkina Ekaterina on 08.05.15.
+//  Copyright (c) 2015 Korovkina Ekaterina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
